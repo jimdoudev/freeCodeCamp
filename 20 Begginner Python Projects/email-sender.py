66665e188713692c5430cf533c70b45cbabd2cc3
Dimitrios Doumpis <doumpis@hotmail.com>
2023-01-16 22:05:23 +0200

    email-sender

---
+++ 20 Begginner Python Projects/email-sender.py
@@ -0,0 +1,22 @@
+from email.message import EmailMessage
+import smtplib
+
+email_sender = 'doumpis@hotmail.com'
+
+email_receiver = 'blablabla@gmail.com'
+
+subject = "Hey!"
+body = """
+I just sent my first mail with Python!
+"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['subject'] = subject
+em.set_content(body)
+
+
+with smtplib.SMTP('smtp.mailtrap.io', 2525) as server:
+    server.login("1f1e9add25dd05", "6f89d0f3660279")
+    server.sendmail(email_sender, email_receiver, em.as_string())
