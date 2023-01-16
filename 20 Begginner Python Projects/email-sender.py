0f242f9324180655069bda013288e2c29384995d
Dimitrios Doumpis <doumpis@hotmail.com>
2023-01-16 22:25:23 +0200

    Word Replacement

--- 20 Begginner Python Projects/email-sender.py
+++ 20 Begginner Python Projects/email-sender.py
@@ -16,7 +16,7 @@ em['To'] = email_receiver
 em['subject'] = subject
 em.set_content(body)
 
-
+#for testing I used an account at mailtrap.io
 with smtplib.SMTP('smtp.mailtrap.io', 2525) as server:
-    server.login("1f1e9add25dd05", "6f89d0f3660279")
+    server.login("1f1e9add25dd05", "6f89d0f3660279") 
     server.sendmail(email_sender, email_receiver, em.as_string())
